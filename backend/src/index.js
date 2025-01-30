d83a7bf71502521031ef8578fdc825b3832ecc4b
Aman Mittal <amanmittal3305@gmail.com>
2025-01-30 18:26:33 +0530

    committed

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,19 +2,17 @@ import express from 'express';
 import authRoutes from './routes/auth.route.js';
 import dotenv from 'dotenv';
 import { connectDB } from './lib/db.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config(); // Load .env variables before using them
 
 const app = express();
+app.use(cookieParser());
 const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
 
 app.listen(PORT, () => {
     console.log('Server is running on Port : ' + PORT);
